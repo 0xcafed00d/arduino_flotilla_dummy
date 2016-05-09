d1579f20b13b6f741db8b92e6a9979a183e46539
Lee Witek <neomech@gmail.com>
2016-05-09 01:53:00 +0100

    added line input

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,40 @@
  * Turns on an LED on for one second,
  * then off for one second, repeatedly.
  */
-#include <Arduino.h>
+#include "Arduino.h"
 
 #define LED 2
 
+const size_t BUFFER_SZ = 128;
+char inputBuffer[BUFFER_SZ];
+size_t bufferPos = 0;
+
+void handleBuffer() {
+	inputBuffer[bufferPos] = 0;
+	Serial.println(inputBuffer);
+	bufferPos = 0;
+}
 
 
 void setup() {
-  Serial.begin(115200);
-
-  // initialize LED digital pin as an output.
-  pinMode(LED, OUTPUT);
+	Serial.begin(115200);
+	pinMode(LED, OUTPUT);
 }
 
+bool toggle = false;
+
 void loop() {
-  Serial.println("hello world");
 
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED, HIGH);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED, LOW);
-
-  // wait for a second
-  delay(1000);
+	int avail = Serial.available();
+	for (int n = 0; n < avail; n++) {
+		digitalWrite(LED, (int) toggle);
+		toggle = !toggle;
+		int c = Serial.read();
+		if (c == '\r') {
+			handleBuffer();
+		}
+		else {
+			inputBuffer[bufferPos++] = (char) c;
+		}
+	}
 }

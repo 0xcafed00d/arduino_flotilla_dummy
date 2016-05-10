75db490d04120a259eedcf8d5a1b46c42d275fee
Lee Witek <neomech@gmail.com>
2016-05-10 11:35:01 +0100

    refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -4,67 +4,66 @@
 
 const size_t BUFFER_SZ = 128;
 char inputBuffer[BUFFER_SZ];
-char elementIdx[32];
-char elementCount;
+char* elementPtrs[32];
 
 size_t bufferPos = 0;
 
-bool isDelim (char c) {
-	return c == ',' || c == ' ';
+bool isDelim(char c) {
+    return c == ',' || c == ' ';
 }
 
-void parseBuffer () {
-	elementCount = 0;
+void parseBuffer() {
+    char** eptrs = elementPtrs;
+    *eptrs = NULL;
 
-	if (bufferPos > 0 && !isDelim(inputBuffer[0])) {
-		elementIdx[elementCount++] = 0;
-		for (size_t n = 1; n < bufferPos; n++){
-			if (isDelim(inputBuffer[n])) {
-				inputBuffer[n] = 0;
-			} else {
-				if (inputBuffer[n-1] == 0) {
-					elementIdx[elementCount++] = n;
-				}
-			}
-		}
-	}
+    if (bufferPos > 0 && !isDelim(inputBuffer[0])) {
+        *eptrs++ = inputBuffer;
+        for (size_t n = 1; n < bufferPos; n++) {
+            if (isDelim(inputBuffer[n])) {
+                inputBuffer[n] = 0;
+            } else {
+                if (inputBuffer[n - 1] == 0) {
+                    *eptrs++ = inputBuffer+n;
+                }
+            }
+        }
+    }
+    *eptrs = NULL;
 }
-
 
 void handleBuffer() {
-	parseBuffer();
+    inputBuffer[bufferPos] = 0;
+    parseBuffer();
 
-	Serial.println ((int)elementCount);
-	for (int n = 0; n < elementCount; n++){
-		Serial.println(inputBuffer + elementIdx[n]);
-	}
+    for (int n = 0; elementPtrs[n]; n++) {
+        Serial.println(elementPtrs[n]);
+    }
 
-	bufferPos = 0;
+    bufferPos = 0;
 }
 
-
 void setup() {
-	Serial.begin(115200);
-	pinMode(LED, OUTPUT);
+    Serial.begin(115200);
+    pinMode(LED, OUTPUT);
 }
 
 bool toggle = false;
 
 void loop() {
 
-	int avail = Serial.available();
-	for (int n = 0; n < avail; n++) {
-		digitalWrite(LED, (int) toggle);
-		toggle = !toggle;
-		int c = Serial.read();
-		switch (c) {
-			case '\r':
-				handleBuffer();
-				break;
-			case '\n':
-				break;
-			default:
-				inputBuffer[bufferPos++] = (char) c;
-		}
-	}
+    int avail = Serial.available();
+    for (int n = 0; n < avail; n++) {
+        digitalWrite(LED, (int) toggle);
+        toggle = !toggle;
+        int c = Serial.read();
+        switch (c) {
+            case '\r':
+                handleBuffer();
+                break;
+            case '\n':
+                break;
+            default:
+                inputBuffer[bufferPos++] = (char) c;
+        }
+    }
 }
